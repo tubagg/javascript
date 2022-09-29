0bf91e5813625efbb06e594dbbec13b77b750b8e
Tuba Bozkurt <tubzkrt@gmail.com>
2022-09-29 10:39:40 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,16 @@
+let email ="sdafgf@gmail.com"
+let FirstName ="tuba"
+let LastName ="Bozkurt"
+
+console.log(email.length)
+//ilk karakteri bulmak
+console.log(FirstName[0])
+//büyük harf kucuk harf
+FirstName=FirstName.toUpperCase()
+console.log(FirstName)
+//arama yapmak
+console.log(email.search("@"))
+console.log(email[15])
+//bilgi degiştirme
+email = email.replace('gmail.com','tubzkrt')
+console.log(email)
